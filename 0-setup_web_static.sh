09e14fb321198b75d39fb59a1a1b5f1c22f0bb8b
Nancy Idiong <80394179+Nancyiddy@users.noreply.github.com>
2023-03-23 12:59:43 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,20 @@
-#!/usr/bin/env bash                                                                                                                                      
-# Setup a web servers for the deployment of web_static.                                                                                                  
-sudo apt update -y                                                                                                                                       
-sudo apt install -y nginx                                                                                                                                
-sudo mkdir -p /data/web_static/releases/test/                                                                                                            
-sudo mkdir -p /data/web_static/shared/                                                                                                                   
-sudo echo "<!DOCTYPE html>                                                                                                                               
-<html>                                                                                                                                                   
-  <head>                                                                                                                                                 
-  </head>                                                                                                                                                
-  <body>                                                                                                                                                 
-    <p>Nginx server test</p>                                                                                                                             
-  </body>                                                                                                                                                
-</html>" | tee /data/web_static/releases/test/index.html                                                                                                 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current                                                                                     
-sudo chown -R ubuntu:ubuntu /data                                                                                                                        
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default                                                                                                             
-sudo service nginx restart
+#!/usr/bin/env bash
+# Setup web servers for deployment of web_static
+apt-get -y install
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
+     <head>
+     </head>
+     <body>
+	Best School School
+     </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
