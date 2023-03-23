56a22ff00e058eeae4917fab9630e3729e3778f6
Nancy Idiong <80394179+Nancyiddy@users.noreply.github.com>
2023-03-23 09:59:56 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,22 +12,12 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models
-    Attributes:
-        id (sqlalchemy String): The BaseModel id.
-        created_at (sqlalchemy DateTime): The datetime at creation.
-        updated_at (sqlalchemy DateTime): The datetime of last update.
+    """This class will defines all common attributes/methods
+    for other classes
     """
-    id = Column(String(60),
-                nullable=False,
-                primary_key=True,
-                unique=True)
-    created_at = Column(DATETIME,
-                        nullable=False,
-                        default=datetime.utcnow())
-    updated_at = Column(DATETIME,
-                        nullable=False,
-                        default=datetime.utcnow())
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
